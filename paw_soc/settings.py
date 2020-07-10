69b2600c09f3578113bf27f2e385ca19c768dbe0
kervo <kervinarias@gmail.com>
2020-07-10 12:18:07 +0000

    home app and templates set up

--- paw_soc/settings.py
+++ paw_soc/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 
@@ -59,7 +60,10 @@ ROOT_URLCONF = 'paw_soc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
