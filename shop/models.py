bed7006b60136592be1569a520e97100aeff6c23
kervo <kervinarias@gmail.com>
2020-07-15 11:35:58 +0000

    fixed typo on category model

--- shop/models.py
+++ shop/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=250)
-    friendly_name = models.CharField(max_lenght=250, null=True, blank=True)
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.name
