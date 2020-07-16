705ac098c5f82e54af969f47b90485406a2ccb05
kervo <kervinarias@gmail.com>
2020-07-16 10:16:23 +0000

    shop sections template and data working with front-end

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name="products")
+    path('', views.all_products, name='products')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Product
 
+# Create your views here.
 
 def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
 
     context = {
         'products': products,
     }
-    
-    return render(request, 'shop/products.html')
+
+    return render(request, 'products/products.html', context)
