6325127099089d778204baeabfa5e4587f6f3165
kervo <kervinarias@gmail.com>
2020-07-16 09:20:47 +0000

    syntax error fixed

--- shop/models.py
+++ shop/models.py
@@ -14,15 +14,12 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-<<<<<<< HEAD
     sku = models.CharField(max_length=250, null=True, blank=True)
     name = models.CharField(max_length=250)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=900, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-=======
->>>>>>> a8d26ed207402f1309652bd447c1945304a3f539
 
     def __str__(self):
         return self.name
