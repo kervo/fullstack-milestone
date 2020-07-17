62e7bc534cee6eb9690d895932973b6ac4d31fd9
kervo <kervinarias@gmail.com>
2020-07-17 07:14:55 +0000

    Added single page for product details

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -11,3 +11,14 @@ def all_products(request):
     }
     """ call the folder from shop > templates > ... """
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/single_product.html', context)
