6bf7ca52994eb77e7ec7408b11051b8f09a138d5
kervo <kervinarias@gmail.com>
2020-07-17 11:31:08 +0000

    search engine test ok

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -6,8 +8,19 @@ def all_products(request):
 
     products = Product.objects.all()
 
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Not sure? Try training toys")
+                return redirect(reverse('products'))
+            """ icontains/idescription makes the search ignore case insensitive """
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
     """ call the folder from shop > templates > ... """
     return render(request, 'products/products.html', context)
