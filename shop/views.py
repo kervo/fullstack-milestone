247f1f5caf48f8d11f70c337192746d19e662e2d
kervo <kervinarias@gmail.com>
2020-07-20 18:00:56 +0000

    categories added on shop section

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Not sure? Try training toys")
+                messages.error(request, "Not sure? Try training toys!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -22,6 +28,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
     """ call the folder from shop > templates > ... """
     return render(request, 'products/products.html', context)
