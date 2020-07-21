f47ebac609d511db18d8d339cae83665ceee157e
kervo <kervinarias@gmail.com>
2020-07-21 08:45:36 +0000

    added function add_to_bag function on single_product page

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ shopping bag template """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ adding items to shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
