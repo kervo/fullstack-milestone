f12f5d724b14661cac1eeff1b3eb045c7d199f56
kervo <kervinarias@gmail.com>
2020-07-23 12:38:01 +0000

    layout and styling added to bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -23,10 +23,10 @@ def bag_contents(request):
 
     if total < settings.FREE_TRIM_TRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_TRIM_TRESHOLD - total
+        free_trim_delta = settings.FREE_TRIM_TRESHOLD - total
     else:
         delivery = 0
-        free_delivery_delta = 0
+        free_trim_delta = 0
 
     grand_total = delivery + total
 
@@ -35,7 +35,7 @@ def bag_contents(request):
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
+        'free_trim_delta': free_trim_delta,
         'free_trim_treshold': settings.FREE_TRIM_TRESHOLD,
         'grand_total': grand_total,
     }
