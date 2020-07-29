8d79e2e5a90040101efe798aae53ca4c034ece85
kervo <kervinarias@gmail.com>
2020-07-29 10:43:55 +0000

    fixed update and remove functions on bag app

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 
 
@@ -23,3 +23,34 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    # updates item on the shopping bag
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    # updates item on the shopping bag
+
+    bag = request.session.get('bag', {})
+
+    if 'item_id':
+        bag.pop(item_id)
+
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
