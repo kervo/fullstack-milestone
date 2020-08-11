37834c255bd98446b2000c489a5e4619121602b2
kervo <52179707+kervo@users.noreply.github.com>
2020-08-11 15:57:29 +0100

    fixed typo line 124

--- paw_soc/settings.py
+++ paw_soc/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'paw_soc.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABES_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
